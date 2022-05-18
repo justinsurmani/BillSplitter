d489c279b0db529ae8894d3e12e8f554d179e820
Justin Surmani <justinsurmani@Justins-MacBook-Air.local>
2022-05-18 14:28:09 -0700

    Initial Commit

---
+++ app/src/main/java/edu/ucsb/cs/cs184/group9/billsplitter/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs184.group9.billsplitter.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
